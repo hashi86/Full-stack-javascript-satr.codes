b5e51c4a87482b22a5c6b627935d28fa64970559
Mohamed Hashi <59652143+hashi86@users.noreply.github.com>
2021-11-13 19:31:34 +0300

    2 . homwork / Creating variables

---
+++ First week/Sunday/My homework/Main.js/main2.js
@@ -0,0 +1,23 @@
+console.log("Week 1 - Day 2 HW");
+
+console.log("Tuwaiq Academy   JS");
+
+let var_1 = 8;
+
+let var_2 = var_1 * 4;
+
+let var_3 = var_1 + var_2;
+
+let var_4 = var_3 + var_2;
+
+let var_5 = "Mohamed";
+
+let var_6 = "hashi";
+
+let var_7 = 32;
+
+let var_8 = "my name is: " + var_5 + " " + var_6 + " my age is: " + var_7;
+
+let var_9 = var_8.length;
+
+let var_11 = var_8[8];

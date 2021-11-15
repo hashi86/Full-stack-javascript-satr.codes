a68623e9c4c9b35f3c2dc443cbe6e184f5385223
Mohamed Hashi <59652143+hashi86@users.noreply.github.com>
2021-11-15 09:31:35 +0300

    homework 3 day

---
+++ First week/Sunday/My homework/Main.js/hw3.js
@@ -0,0 +1,43 @@
+console.log("Week 1 - Day 3 HW");
+
+console.log("Tuwaiq Academy   JS");
+
+
+
+let hw_D03_1 = 1 - 3 + 6> 1-8;
+
+//                        T
+
+
+let hw_D03_2 = 2 * (3 + 9 ) >=9-1 * 0
+
+//                        T
+
+let hw_D03_3 = (6 / 2 ) * 5 <=1-5
+
+//                        F
+
+let hw_D03_4 = 15% 2>9-1
+//                        F
+
+
+let hw_D03_5 = 7 * 7 ==='4'+'9'
+//                         F
+
+let hw_D03_6 = 7 * 7 =='4'+'9'
+//                        T
+
+let hw_D03_7 = 7 *7 !=='4'+'9'
+//                        T
+
+let hw_D03_8 =  7 * 7 !='4'+'9'
+//                        F
+
+let hw_D03_9 = 7>'7'
+//                        F
+
+let hw_D03_10 = 7<='7'
+//                        T
+
+let hw_D03_11 = 9 + 2 >8-5 * 9
+//                        T

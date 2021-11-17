faebfacb969238fdfa31747d932ef3afb6996181
Mohamed Hashi <59652143+hashi86@users.noreply.github.com>
2021-11-17 23:04:42 +0300

    homework 4 day

---
+++ First week/Sunday/My homework/Main.js/hw4.js
@@ -0,0 +1,110 @@
+console.log("Week 1 - Day 4 HW");
+
+console.log("Tuwaiq Academy   JS");
+
+
+
+function printNameAndAge(name,age) {
+
+console.log('my name is: '   +  name + ' and my age is:' , age);
+
+    
+}
+
+
+printNameAndAge('mohamed',27)
+
+// my name is: mohamed and my age is: 27
+
+printNameAndAge('Jouza',23)
+
+// my name is: Jouza and my age is: 23
+
+printNameAndAge('Ali',35)
+
+// my name is: Ali and my age is: 35
+
+
+function subtract_1(a,b) {
+
+    return b-a
+    
+}
+
+console.log(subtract_1(5,2));
+
+// -3
+console.log(subtract_1(7,3));
+// -4
+
+console.log(subtract_1(2,7));
+// 5
+
+
+function multi_1(a,b) {
+
+    return a*b
+    
+}
+
+console.log(multi_1(5,2));
+
+// 10
+console.log(multi_1(7,3));
+// 21
+
+console.log(multi_1(2,7));
+// 14
+
+
+   function mod_3(number) {
+
+        return number %3
+       
+   }
+
+   console.log(mod_3(6));
+//   0
+
+   console.log(mod_3(8));
+//    2
+
+   console.log(mod_3(13));
+//    1
+
+   
+  function stringLong(str) {
+
+      return str.length
+      
+  }
+
+     console.log(stringLong('Tuwaiq_1000'))
+//         11 
+
+     console.log(stringLong('moh Jouza'))
+    //     9
+
+     console.log(stringLong('javascript'))
+
+    //     10
+
+
+    function firstChar(string) {
+
+
+        return string[0]
+        
+    }
+
+    console.log(firstChar('Tuwaiq_1000'))
+
+//          T
+    console.log(firstChar('moh Jouza'))
+
+//          M
+    console.log(firstChar('javascript'))
+
+    //       j
+
+    
